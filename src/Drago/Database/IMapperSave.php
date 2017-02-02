dd4a826ef9e4dd52d49d3339d412e236d3158f5e
Zdeněk Papučík <zdenek.papucik@gmail.com>
2017-02-02 12:19:05 +0100

    Create IMapperSave.php

---
+++ src/Drago/Database/IMapperSave.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Extending for Nette Framework.
+ * Copyright (c) 2015, Zdeněk Papučík
+ *
+ * @package Drago
+ */
+namespace Drago\Database;
+
+/**
+ * Database mapper.
+ * @author Zdeněk Papučík
+ */
+interface IMapperSave
+{
+	/**
+	 *  Save or update records.
+	 * @param mixed
+	 */
+	public function save($entity);
+
+}
