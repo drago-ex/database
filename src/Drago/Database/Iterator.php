9914a651a44c9963a4df5aef3070e701fcd54bd5
Zdeněk Papučík <zdenek.papucik@gmail.com>
2017-11-10 11:52:07 +0100

    Update Iterator.php

--- src/Drago/Database/Iterator.php
+++ src/Drago/Database/Iterator.php
@@ -5,24 +5,47 @@
  * Copyright (c) 2015, Zdeněk Papučík
  */
 namespace Drago\Database;
+
 use Nette;
+use Nette\Utils;
 
 /**
- * Entity iterator.
+ * Iterator entity or convert lower/upper keys.
  */
 class Iterator
 {
 	use Nette\StaticClass;
 
+    	/**
+	 * Convert keys to lower.
+	 * @var string
+	 */
+	const LOWER = 'lower';
+
 	/**
+	 * Convert keys to upper.
+	 * @var string
+	 */
+	const UPPER = 'upper';
+
+	/**
+	 * Convert entity and keys.
 	 * @param mixed
+	 * @param string
 	 * @return array
 	 */
-	public static function set($entity)
+	public static function set($entity, $convert = null)
 	{
 		$items = [];
 		foreach ($entity as $key => $item) {
-			$items[$key] = $item;
+			switch ($convert) {
+				case 'lower': $items[$convert ? Utils\Strings::lower($key) : $key] = $item; break;
+				case 'upper': $items[$convert ? Utils\Strings::upper($key) : $key] = $item; break;
+				default:
+					$items[$key] = $item;
+				break;
+			}
+
 		}
 		return $items;
 	}
