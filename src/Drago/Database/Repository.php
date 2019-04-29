18b0f3fd136a5bdda22001f48997b9446fa0e9dd
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-04-29 06:49:04 +0200

    Update Repository.php

--- src/Drago/Database/Repository.php
+++ src/Drago/Database/Repository.php
@@ -6,27 +6,62 @@ declare(strict_types = 1);
  * Drago Database
  * @copyright Zdeněk Papučík
  */
-namespace Drago\Database;
+namespace App\Presenters;
+
+use Dibi\Connection;
+use Dibi\Fluent;
+use stdClass;
 
 /**
- * Repository base for iterate entity.
+ * Repository base.
+ * @property-read Connection|stdClass $db
  * @package Drago\Database
  */
-trait Repository
+trait BaseRepository
 {
 	/**
-	 * Convert entity to array.
-	 * @param  object $entity
-	 * @return array
+	 * Get all records.
 	 */
-	public function toArray($entity)
+	public function get(): Fluent
 	{
-		$arr = [];
-		foreach ($entity as $key => $value) {
-			if (!is_null($value)) {
-				$arr[$key] = $value;
-			}
-		}
-		return $arr;
+		return $this->db
+			->select('*')
+			->from($this->table);
+	}
+
+
+	/**
+	 * Find record.
+	 */
+	public function find(int $id): Fluent
+	{
+		return $this->get()
+			->where("{$this->primaryId} = ?", $id);
+	}
+
+
+	/**
+	 * Remove record.
+	 */
+	public function remove(int $id): Fluent
+	{
+		return $this->db
+			->delete($this->table)
+			->where("{$this->primaryId} = ?", $id);
+	}
+
+
+	/**
+	 * Insert or update record.
+	 */
+	public function save(array $args, int $id = null): Fluent
+	{
+		$update = $this->db
+			->update($this->table, $args)
+			->where("{$this->primaryId} = ?", $id);
+
+		$insert = $this->db->insert($this->table, $args);
+		$id ? $row = $update : $row = $insert;
+		return $row;
 	}
 }
