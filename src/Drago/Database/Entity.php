ea8d5d13c13109bdd79a2ae421ee1f23e979443e
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-06-03 09:03:42 +0200

    Create Entity.php

---
+++ src/Drago/Database/Entity.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types = 1);
+
+/**
+ * Drago Database
+ * @copyright Zdeněk Papučík
+ */
+namespace Drago\Database;
+
+use ArrayAccess;
+use ArrayIterator;
+use IteratorAggregate;
+use Countable;
+
+/**
+ * Class Entity
+ * @package Drago\Database\Entity
+ */
+class Entity implements ArrayAccess, IteratorAggregate, Countable
+{
+	/** @var mixed */
+	protected $data;
+
+	/** @var mixed */
+	protected $modify;
+
+
+	public function __construct(array $arr = [])
+	{
+		$this->data = $arr;
+		foreach ($arr as $k => $v) {
+			$this->$k = $v;
+		}
+	}
+
+
+	/**
+	 * Convert to array.
+	 */
+	public function toArray(): array
+	{
+		return (array) $this->data;
+	}
+
+
+	/**
+	 * Modified data.
+	 */
+	public function getModify(): ?array
+	{
+		return $this->modify;
+	}
+
+
+	/**
+	 * Retrieve an external iterator.
+	 */
+	public function getIterator()
+	{
+		return new ArrayIterator($this);
+	}
+
+
+	/**
+	 * Whether a offset exists.
+	 */
+	public function offsetExists($offset)
+	{
+		return isset($this->$offset);
+	}
+
+
+	/**
+	 * Offset to retrieve.
+	 */
+	public function offsetGet($offset)
+	{
+		return $this->$offset;
+	}
+
+
+	/**
+	 * Offset to set.
+	 */
+	public function offsetSet($offset, $value)
+	{
+		$this->modify[$offset] = $value;
+		$this->$offset = $value;
+	}
+
+
+	/**
+	 * Offset to unset.
+	 */
+	public function offsetUnset($offset)
+	{
+		unset($this->$offset);
+	}
+
+
+	/**
+	 * Count elements of an object.
+	 */
+	public function count()
+	{
+		return count((array) $this);
+	}
+}
