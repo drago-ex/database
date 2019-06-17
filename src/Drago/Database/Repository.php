e4777e111f4ef799aca05553a7b25d5e4eca443b
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-06-17 10:32:31 +0200

    Update Repository.php

--- src/Drago/Database/Repository.php
+++ src/Drago/Database/Repository.php
@@ -64,7 +64,7 @@ trait Repository
 	 * @return Result|int|null
 	 * @throws Dibi\Exception
 	 */
-	public function erase(string $cond, ...$parm)
+	public function remove(string $cond, ...$parm)
 	{
 		return $this->db
 			->delete($this->table)
@@ -78,7 +78,7 @@ trait Repository
 	 * @return Result|int|null
 	 * @throws Dibi\Exception
 	 */
-	public function eraseId(int $id)
+	public function removeId(int $id)
 	{
 		return $this->delete($this->primaryId, $id);
 	}
