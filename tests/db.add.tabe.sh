07dbda4f0c9d23feb647c6bba4f37cee9f6f11a5
accgit <zdenek.papucik@gmail.com>
2019-10-14 10:39:37 +0200

    Fix bash path

--- tests/db.add.tabe.sh
+++ tests/db.add.tabe.sh
@@ -3,8 +3,5 @@ export ORACLE_SID=XE;
 $ORACLE_HOME/bin/sqlplus -S travis/travis <<SQL
 whenever sqlerror exit 2;
 CREATE SEQUENCE test_seq;
-CREATE TABLE test (
-  sample_id NUMBER DEFAULT TEST_SEQ.nextval NOT NULL PRIMARY KEY,
-  sample_string VARCHAR2(255) NOT NULL
-);
+CREATE TABLE test (sample_id NUMBER DEFAULT TEST_SEQ.nextval NOT NULL PRIMARY KEY,sample_string VARCHAR2(255) NOT NULL);
 SQL
