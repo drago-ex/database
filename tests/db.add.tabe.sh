8ea682c5e405659a0025c5b9988669b41d380736
Zdeněk Papučík <zdenek.papucik@gmail.com>
2019-10-15 06:15:43 +0200

    Update db.add.tabe.sh

--- tests/db.add.tabe.sh
+++ tests/db.add.tabe.sh
@@ -2,24 +2,19 @@ export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe;
 export ORACLE_SID=XE;
 $ORACLE_HOME/bin/sqlplus -S travis/travis <<SQL
 whenever sqlerror exit 2;
+CREATE SEQUENCE test_seq;
+
 CREATE TABLE test (sample_id INTEGER NOT NULL PRIMARY KEY, sample_string VARCHAR2(255) NOT NULL);
-SQL
-$ORACLE_HOME/bin/sqlplus -S travis/travis <<SQL
-whenever sqlerror exit 2;
-CREATE SEQUENCE test_id_seq;
-SQL
-$ORACLE_HOME/bin/sqlplus -S travis/travis <<SQL
-whenever sqlerror exit 2;
+
 CREATE TRIGGER test_on_insert
-BEFORE INSERT ON test
-FOR EACH ROW
+  BEFORE INSERT OR UPDATE ON test
+  FOR EACH ROW
+
 BEGIN
-  SELECT test_id_seq.nextval
+  SELECT test_seq.nextval
   INTO :new.sample_id
   FROM dual;
 END;
-SQL
-$ORACLE_HOME/bin/sqlplus -S travis/travis <<SQL
-whenever sqlerror exit 2;
-INSERT INTO test (sample_string) VALUES ('Hello');
+/
+
 SQL
