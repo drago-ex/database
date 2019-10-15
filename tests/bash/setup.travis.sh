df50fbfae02091bba90853ebceb30ddb5466049a
accgit <zdenek.papucik@gmail.com>
2019-10-15 09:01:41 +0200

    Added test change travis settings

--- tests/bash/setup.travis.sh
+++ tests/bash/setup.travis.sh
@@ -3,7 +3,6 @@ wget https://raw.githubusercontent.com/accgit/docker/master/docker-oracle/oracle
 wget https://raw.githubusercontent.com/accgit/docker/master/docker-oracle/oracle/oracle-instantclient12.2-devel_12.2.0.1.0-2_amd64.deb
 
 bash accept_the_license_agreement_for_oracledb_xe_11g_and_install.sh
-bash add.tabe.sh
 
 sudo dpkg -i oracle-instantclient12.2-basic_12.2.0.1.0-2_amd64.deb
 sudo dpkg -i oracle-instantclient12.2-devel_12.2.0.1.0-2_amd64.deb
