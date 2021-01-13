2b75e2ad958969846602febf21a294b00f295f90
Zdeněk Papučík <zdenek.papucik@gmail.com>
2021-01-13 08:07:17 +0100

    Update db.sql

--- tests/db.sql
+++ tests/db.sql
@@ -6,13 +6,3 @@ CREATE TABLE `test` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO `test` (`sampleId`, `sampleString`) VALUES (1, 'Hello');
-
-
-DROP TABLE IF EXISTS `test_entity`;
-CREATE TABLE `test_entity` (
-  `sampleId` int(11) NOT NULL AUTO_INCREMENT,
-  `sampleString` varchar(255) CHARACTER SET utf8 NOT NULL,
-                        PRIMARY KEY (`sampleId`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-INSERT INTO `test_entity` (`sampleId`, `sampleString`) VALUES (1, 'Hello');
