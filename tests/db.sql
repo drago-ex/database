87266d541310e56d22be035894b9b2a279a5753f
Zdeněk Papučík <zdenek.papucik@gmail.com>
2021-09-16 11:53:48 +0200

    Update db.sql

--- tests/db.sql
+++ tests/db.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS `test`;
 CREATE TABLE `test` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `sample` varchar(255) CHARACTER SET utf8 NOT NULL,
-  PRIMARY KEY (`sampleId`)
+  PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 INSERT INTO `test` (`id`, `sample`) VALUES (1, 'Hello');
