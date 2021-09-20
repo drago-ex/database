2ba867cb4aa819f514d6cd0d165cbf6da87be27d
Zdeněk Papučík <zdenek.papucik@gmail.com>
2021-09-20 13:16:06 +0200

    Update TestEntity.php

--- tests/TestEntity.php
+++ tests/TestEntity.php
@@ -5,8 +5,8 @@ declare(strict_types=1);
 
 class TestEntity extends Drago\Database\Entity
 {
-	const TABLE = 'test';
-	const PRIMARY = 'id';
+	public const TABLE = 'test';
+	public const PRIMARY = 'id';
 
 	public ?int $id;
 	public string $sample;
