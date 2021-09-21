fcfbc173484d98dab6b3dbb02c584d44dd9bccb7
Zdeněk Papučík <zdenek.papucik@gmail.com>
2021-09-21 09:26:42 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,15 +2,13 @@
 
 declare(strict_types=1);
 
-use Tester\Environment;
-
 if (@!include __DIR__ . '/../vendor/autoload.php') {
 	echo 'Install Nette Tester using `composer install`';
 	exit(1);
 }
 
 
-Environment::setup();
+Tester\Environment::setup()
 date_default_timezone_set('Europe/Prague');
 
 
