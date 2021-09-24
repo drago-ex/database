0199ecfe4ba3fb917e292dd22979b27fa85c8632
Zdeněk Papučík <zdenek.papucik@gmail.com>
2021-09-24 16:08:33 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,6 +11,20 @@ if (@!include __DIR__ . '/../vendor/autoload.php') {
 Tester\Environment::setup();
 date_default_timezone_set('Europe/Prague');
 
+const TEMP_DIR = __DIR__ . '/tmp';
+
+@mkdir(dirname(TEMP_DIR));
+@mkdir(TEMP_DIR);
+
+$boot = new Configurator;
+$boot->setTempDirectory(TEMP_DIR);
+$boot->createRobotLoader()
+	->addDirectory(__DIR__)
+	->addDirectory(__DIR__ . '/../src')
+	->register();
+
+return $boot->createContainer();
+
 
 function test(string $title, Closure $function): void
 {
