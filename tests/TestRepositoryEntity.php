18fd7537e79aaea94acdb0ca47d982b812566563
accgit <zdenek.papucik@gmail.com>
2022-04-22 07:07:26 +0200

    Fix tests

--- tests/TestRepositoryEntity.php
+++ tests/TestRepositoryEntity.php
@@ -2,10 +2,15 @@
 
 declare(strict_types=1);
 
+use Dibi\Connection;
 use Drago\Attr\Table;
 
 
 #[Table(TestEntity::TABLE, TestEntity::PRIMARY)]
-class TestRepositoryEntity extends Drago\Database\Connect
+class TestRepositoryEntity extends Drago\Database\Repository
 {
+	public function __construct(
+		public Connection $db,
+	) {
+	}
 }
