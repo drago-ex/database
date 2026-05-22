7accb78d9746c7a29d1d5911e477e30f9181179c
Zdeněk Papučík <zdenek.papucik@gmail.com>
2026-05-22 10:17:18 +0200

    Update

---
+++ ecs.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+	'skip' => [
+		'SlevomatCodingStandard.PHP.RequireExplicitAssertion.RequiredExplicitAssertion' => [
+			__DIR__ . '/src/Drago/Database/ExtraFluent.php',
+		],
+	],
+];
